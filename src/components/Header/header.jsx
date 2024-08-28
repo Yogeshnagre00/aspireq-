70c97365148b9459a9163a78a8c84374eb9c4276
Yogesh Nagre <yogeshnagre000@gmail.com>
2024-08-28 19:46:50 +0530

    add lets talk

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,16 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./header.css";
 
 const Navbar = () => {
   const location = useLocation(); // Get the current location
+  const navigate = useNavigate(); // Hook for programmatic navigation
+
+  const handleLetsTalkClick = () => {
+    // Redirect to "/contactUs" when the button is clicked
+    if (location.pathname !== "/contactUs") {
+      navigate("/contactUs");
+    }
+  };
 
   return (
     <header>
@@ -12,7 +20,7 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <a href="/">HOME</a>
+            <Link to="/">HOME</Link>
           </li>
           {/* Dropdown for SERVICES */}
           <li className="dropdown">
@@ -34,7 +42,7 @@ const Navbar = () => {
           </li>
           {/* Dropdown for ABOUT US */}
           <li className="dropdown">
-            <a href="#">ABOUT Us</a>
+            <a href="#">ABOUT US</a>
             <ul className="dropdown-menu">
               <li>
                 <a href="#">Our Team</a>
@@ -48,23 +56,17 @@ const Navbar = () => {
             </ul>
           </li>
           <li>
-            <Link to="/contactUs">CONTACT Us</Link>
+            <Link to="/contactUs">CONTACT US</Link>
           </li>
         </ul>
-        {/* Conditionally render the button text and functionality based on the current path */}
-        {location.pathname === "/" ? (
-          <a
-            href="#"
-            className="btn"
-            onClick={() => alert("Click of Let's Talk")}
-          >
-            Let&apos;s Talk &rarr;
-          </a>
-        ) : (
-          <Link to="/" className="btn">
-            Home Page
-          </Link>
-        )}
+        {/* Conditionally apply a class to the button based on the current path */}
+        <a
+          href="#"
+          className={`btn ${location.pathname === "/" ? "" : "hidden"}`}
+          onClick={handleLetsTalkClick}
+        >
+          Let&apos;s Talk &rarr;
+        </a>
       </nav>
     </header>
   );
